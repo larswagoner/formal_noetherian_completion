994e65c12e4e16efcb115f53fbb582cf82d851ae
BuildTools <unconfigured@null.spigotmc.org>
2025-04-05 00:28:40 +0200

    Changed main theorem formulation

--- formal_proofs/main.lean
+++ formal_proofs/main.lean
@@ -5,7 +5,7 @@ import Mathlib
 
 
 
--- 10.22(i) 
+-- 10.22(i)
 -- inspiration GradedRing.GradeZero.isNoetherianRing
 
 --lemma GradedRing.ofNoetherian.isNoetherian {A: Type*} [Ring A] [IsNoetherianRing A]
@@ -14,7 +14,5 @@ import Mathlib
 -- 10.25
 
 -- 10.26
-theorem AdicCompletion.ofNoetherianRing.isNoetherianRing {A : Type*} [CommRing A] (I : Ideal A) [h: IsNoetherianRing A] : IsNoetherian A (AdicCompletion I A) := by 
+theorem AdicCompletion.ofNoetherianRing.isNoetherianRing {A : Type*} [CommRing A] (I : Ideal A) [h: IsNoetherianRing A] : IsNoetherianRing (AdicCompletion I A) := by
   sorry
-
-
