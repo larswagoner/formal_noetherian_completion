77fcc1ab9fb0292b649711c216f8b5819a2a12b0
Lars Wagoner <larswagoner@protonmail.com>
2025-05-11 00:53:28 +0200

    enough progress on components for now to make progress on am_10_22

--- MyProject/AssociatedGradedRing/Components.lean
+++ MyProject/AssociatedGradedRing/Components.lean
@@ -6,7 +6,6 @@ variable {A : Type u} [CommRing A] (I : Ideal A)
 -- goal prove that I^m/I^m+1 ≃+ GRP I m, maybe treat case of m=0 differently since ring structure
 
 /-- `A/I` is isomorphic to `GradedRingPiece I 0` as rings-/
-
 def zero_toFun_aux₁ : A →+ (CanonicalFiltration I).N 0 where
   toFun := (fun a => ⟨ a , by simp ⟩)
   map_zero' := rfl
@@ -47,66 +46,40 @@ def zero_invFun : (GradedRingPiece I 0) →+* A ⧸ I where
     rfl
   
 
-
-
-def zeroeth_isomorphism  : A ⧸ I ≃+* (GradedRingPiece I 0) where
+def GradedRingPiece_zero_isomorphism (I : Ideal A): A ⧸ I ≃+* (GradedRingPiece I 0) where
   __ := zero_toFun I
   invFun := zero_invFun I
-  left_inv := sorry
-  right_inv := sorry
-
-  
-
-
-def aux1 : A →+ (CanonicalFiltration I).N 0 where
-  toFun := (fun a => ⟨ a , by simp ⟩)
-  map_zero' := rfl
-  map_add' := fun _ _ => rfl
-
-
-
--- we need A/I  ≃+* GRP I 0 , the sorries below may not be necessary, nonetheless the general case of I^m/I^m+1 ≃+ GRP I m should be proven. 
-
-def aux2 : A ⧸ I →+ (GradedPiece (CanonicalFiltration I) 0) := by
-  apply QuotientAddGroup.map _ _ (aux1 I) _
-  · intro x hx
+  left_inv := by 
     simp
-    exact hx
-
-
-def zeroeth_morphism : A ⧸ I →+* (GradedPiece (CanonicalFiltration I) 0) where
-  toFun := aux2 I
-  map_one' := rfl
-  map_mul' := by
-    rintro ⟨x⟩ ⟨y⟩ 
+    unfold Function.LeftInverse
+    rintro ⟨x⟩ 
     rfl
-  map_zero' := AddMonoidHom.map_zero (aux2 I)
-  map_add' := fun x y => AddMonoidHom.map_add (aux2 I) x y
-
-
-
-
-def aux11 (I : Ideal A) (m : ℕ ): (CanonicalFiltration I).N m →+  ↥(I^m) := sorry
-
-lemma aux1.Bijective (I : Ideal A) (m : ℕ ) : Function.Bijective (idealPowerToFiltrationComponent I m ):= sorry 
-
-
-
-
-
-noncomputable def GradedRingPiece.toIdealQuotient.map (I : Ideal A) (m : ℕ) : GradedRingPiece I m → I^m/I^(m+1) := by
-  unfold GradedRingPiece GradedPiece CanonicalFiltration
-  simp 
-  intro x
-  refine Classical.indefiniteDescription (Membership.mem (I ^ m / I ^ (m + 1))) ?_
-  refine Subtype.existsOfSubtype ?_
-
+  right_inv := by
+    simp
+    unfold Function.RightInverse Function.LeftInverse
+    rintro ⟨x , hx⟩
+    simp
+    rfl
   
-  sorry
-
-noncomputable def GradedRingPiece.toIdealQuotient (I : Ideal A) (m : ℕ) : GradedRingPiece I m →+ I^m/I^(m+1) where
-  toFun := GradedRingPiece.toIdealQuotient.map I m
+/-- `Iᵐ/Iᵐ⁺¹` is isomorphic to `GradedRingPiece I m` as modules -/
+def m_toFun_aux₁: ↥(I^m) →+ (CanonicalFiltration I).N m where
+  toFun := by -- do this without rewrite!
+    rintro ⟨x, hx⟩
+    rw [Ideal.stableFiltration_N, smul_eq_mul, Ideal.mul_top]
+    exact ⟨x, hx⟩   
   map_zero' := sorry
   map_add' := sorry
 
-lemma GradedRingPiece.toIdealQuotient.isBijective (I : Ideal A) (m : ℕ) : Function.Bijective (GradedRingPiece.toIdealQuotient I m):= sorry
+def m_toFun (I : Ideal A) (m : ℕ) : I^m/I^(m+1) →+ GradedRingPiece I m := sorry
+
+def m_invFun_aux₁:(CanonicalFiltration I).N m →+ ↥(I^m):= sorry
+
+def m_invFun (I : Ideal A) (m : ℕ): GradedRingPiece I m →+ I^m/I^(m+1) := sorry
+
+
+def GradedRingPiece_m_isomorphism (I : Ideal A) (m : ℕ) : I^m/I^(m+1) ≃+ (GradedRingPiece I m) where
+  __ := m_toFun I m
+  invFun := m_invFun I m
+  left_inv := sorry
+  right_inv := sorry
+  
