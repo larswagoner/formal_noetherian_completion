10573df8df02d7aea2bcd079b6c352c503e34c83
Lars Wagoner <larswagoner@protonmail.com>
2025-05-16 00:47:03 +0200

    clear plan for 22 and surj map

--- MyProject/AssociatedGradedRing/Components.lean
+++ MyProject/AssociatedGradedRing/Components.lean
@@ -6,6 +6,8 @@ variable {A : Type u} [CommRing A] (I : Ideal A)
 
 -- goal prove that I^m/I^m+1 ≃+ GRP I m, maybe treat case of m=0 differently since ring structure
 
+open QuotientAddGroup
+
 /-- `A/I` is isomorphic to `GradedRingPiece I 0` as rings-/
 def zero_toFun_aux₁ : A →+ (CanonicalFiltration I).N 0 where
   toFun := (fun a => ⟨ a , by simp ⟩)
@@ -13,106 +15,68 @@ def zero_toFun_aux₁ : A →+ (CanonicalFiltration I).N 0 where
   map_add' := fun _ _ => rfl
 
 
-def zero_toFun_aux₂ : A ⧸ I →+ (GradedRingPiece I 0) := by
-  apply QuotientAddGroup.map _ _ (zero_toFun_aux₁ I) _
-  · intro x hx
-    simp
-    exact hx
-
 def zero_toFun : A ⧸ I →+* (GradedRingPiece I 0) where
-  __ := zero_toFun_aux₂ I
+  __ := map _ _ (zero_toFun_aux₁ I) fun _ _ ↦ by simpa
   map_one' := rfl
   map_mul' := by
     rintro ⟨x⟩ ⟨y⟩ 
     rfl
 
-def zero_invFun_aux₁: (CanonicalFiltration I).N 0 →+ A where
-  toFun := (fun ⟨a, _⟩ => a)
+def zero_invFun_aux₁ : (CanonicalFiltration I).N 0 →+ A where
+  toFun := Subtype.val
   map_zero' := rfl
-  map_add' := fun _ _ => rfl
-
-def zero_invFun_aux₂: GradedRingPiece I 0 →+ A ⧸ I := by
-  apply QuotientAddGroup.map _ _ (zero_invFun_aux₁ I) _
-  intro x hx
-  simp at hx
-  exact hx
+  map_add' _ _ := rfl
 
 def zero_invFun : (GradedRingPiece I 0) →+* A ⧸ I where
-  __ := zero_invFun_aux₂ I
+  __ := map _ _ (zero_invFun_aux₁ I) fun _ h ↦ by simpa using h
   map_one' := rfl
   map_mul' := by
-    rintro ⟨x, hx⟩ ⟨ y, hy⟩ 
-    simp
+    rintro ⟨_⟩ ⟨_⟩
     rfl
 
 def GradedRingPiece_zero_isomorphism (I : Ideal A): A ⧸ I ≃+* (GradedRingPiece I 0) where
   __ := zero_toFun I
   invFun := zero_invFun I
-  left_inv := by 
-    simp
-    unfold Function.LeftInverse
-    rintro ⟨x⟩ 
+  left_inv := by
+    rintro ⟨_⟩ 
     rfl
   right_inv := by
-    simp
-    unfold Function.RightInverse Function.LeftInverse
-    rintro ⟨x , hx⟩
-    simp
+    rintro ⟨_⟩ 
     rfl
 
-
-
-/-- `I/I²` is isomorphic to `GradedRingPiece 1`-/  
-def one_toFun_aux₁: ↥(I) →+ (CanonicalFiltration I).N 1 where
-  toFun := fun a => ⟨a, by simp⟩
-  map_zero' := by simp 
-  map_add' := by simp
-
-
-def one_toFun (I : Ideal A) : I/I^2 →+ GradedRingPiece I 1 where
-  toFun := sorry
-  map_zero' := sorry
-  map_add' := sorry
-  
-
-def one_invFun_aux₁:(CanonicalFiltration I).N 1 →+ I:= sorry
-
-
-def one_invFun (I : Ideal A): GradedRingPiece I 1 →+ I/I^2 := sorry
-
-def GradedRingPiece_one_isomorphism (I : Ideal A): I/I^2 ≃+ (GradedRingPiece I 1) where
-  __ := one_toFun I 
-  invFun := one_invFun I
-  left_inv := sorry
-  right_inv := sorry
 
 
 /-- `Iᵐ/Iᵐ⁺¹` is isomorphic to `GradedRingPiece I m` as modules -/
-def m_toFun_aux₁ (m : ℕ): ↥(I^m) →+ (CanonicalFiltration I).N m where
+def Taux (m : ℕ): ↥(I^m) →+ (CanonicalFiltration I).N m where
   toFun x := ⟨x, by simp⟩
   map_zero' := by simp
   map_add' := by simp
 
-def m_toFun (I : Ideal A) (m : ℕ) : (↥(I^m) ⧸ I • (⊤ : Submodule A ↥(I^m))) →+ GradedRingPiece I m := by
-  apply QuotientAddGroup.map _ _ (m_toFun_aux₁ I m) _
-  rintro ⟨x, hx⟩ p
-  simpa [Submodule.mem_smul_top_iff I, ← pow_succ'] using p
-
-
+def mToFun (I : Ideal A) (m : ℕ) : ↥(I^m) ⧸ I • (⊤ : Submodule A ↥(I^m)) →+ GradedRingPiece I m :=
+  QuotientAddGroup.map _ _ (Taux I m) fun ⟨x, _⟩ p ↦ by 
+    simpa [Submodule.mem_smul_top_iff I, ← pow_succ'] using p
   
 
-def m_invFun_aux₁:(CanonicalFiltration I).N m →+ ↥(I^m)where
+def Iaux (m : ℕ) : (CanonicalFiltration I).N m →+ ↥(I^m) where
   toFun x := ⟨x, by simpa using x.2⟩ 
   map_zero' := by simp
   map_add' := by simp
 
-def m_invFun (I : Ideal A) (m : ℕ): GradedRingPiece I m →+ I^m/I^(m+1) := sorry
+def mInvFun (I : Ideal A) (m : ℕ) : GradedRingPiece I m →+ ↥(I^m) ⧸ I • (⊤ : Submodule A ↥(I^m)) := 
+  QuotientAddGroup.map _ _ (Iaux I m) fun a ha ↦ by 
+    simpa [Submodule.mem_smul_top_iff I, ← pow_succ'] using ha
 
-def GradedRingPiece_m_isomorphism (I : Ideal A) (m : ℕ) : I^m/I^(m+1) ≃+ (GradedRingPiece I m) where
-  __ := m_toFun I m
-  invFun := m_invFun I m
-  left_inv := sorry
-  right_inv := sorry
+def GradedRingPiece_m_iso (I : Ideal A) (m : ℕ) : 
+    ↥(I^m) ⧸ I • (⊤ : Submodule A ↥(I^m)) ≃+ (GradedRingPiece I m) where
+  __ := mToFun I m
+  invFun := mInvFun I m
+  left_inv := (·.inductionOn fun _ ↦ rfl)
+  right_inv x := x.inductionOn fun _ ↦ rfl
+
+
+lemma GradedRingPiece_m_iso.bijective (I : Ideal A) (m : ℕ) : 
+    Function.Bijective (GradedRingPiece_m_iso I m) := AddEquiv.bijective (GradedRingPiece_m_iso I m)
+
 
 
 /-- Module structures -/
@@ -122,8 +86,7 @@ def GradedRingPiece_m_isomorphism (I : Ideal A) (m : ℕ) : I^m/I^(m+1) ≃+ (Gr
 instance : Module A I := by infer_instance
 instance : Module A (I/I^2) := by infer_instance
 instance : Module (GradedRingPiece I 0) (GradedRingPiece I 1) := by infer_instance
-instance : Module (GradedRingPiece I 0) (I/I^2) := sorry
 
 
-instance : Module (A ⧸ I) (I  ⧸  I • (⊤ : Submodule A I) ):=  Module.instQuotientIdealSubmoduleHSMulTop (↥I) I
+instance : Module (A ⧸ I) (I ⧸ I • (⊤ : Submodule A I)):=  Module.instQuotientIdealSubmoduleHSMulTop ↥I I
 
